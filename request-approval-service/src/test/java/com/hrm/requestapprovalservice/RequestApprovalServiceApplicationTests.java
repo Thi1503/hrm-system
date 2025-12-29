c447ef649f0a8758ad8da28f4af71283a4c07252
Thi1503 <thile15032003@gmail.com>
2025-12-29 16:27:27 +0700

    init request-approval-service

---
+++ request-approval-service/src/test/java/com/hrm/requestapprovalservice/RequestApprovalServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hrm.requestapprovalservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RequestApprovalServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
