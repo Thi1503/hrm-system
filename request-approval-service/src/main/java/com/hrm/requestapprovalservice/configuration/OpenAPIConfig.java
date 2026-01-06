60b685a986f772c586cdd2e7aa9f20371cb22ce6
Thi1503 <thile15032003@gmail.com>
2026-01-06 09:20:32 +0700

    hrm-system(configuration): add API docs

---
+++ request-approval-service/src/main/java/com/hrm/requestapprovalservice/configuration/OpenAPIConfig.java
@@ -0,0 +1,26 @@
+package com.hrm.requestapprovalservice.configuration;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.models.OpenAPI;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Request Approval Service",
+                version = "v0.0.1",
+                description = "Request Approval Service API"
+        )
+)
+public class OpenAPIConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new io.swagger.v3.oas.models.info.Info()
+                        .title("Request Approval Service")
+                        .description("Request Approval Service API")
+                        .version("v0.0.1"));
+    }
+}
